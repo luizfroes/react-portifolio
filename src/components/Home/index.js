442311158e8d595b504918cffc5f15513dd77576
Luiz Carlos Froes <luizfroes@gmail.com>
2022-02-16 01:35:49 +0000

    Added Home

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../../style.css";
+import { NavBar } from "../NavBar";
+
+export const Home = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};

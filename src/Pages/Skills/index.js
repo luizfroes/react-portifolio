be8770fd6a8628131b07959bea9dce8ab7b77020
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-02 15:46:38 +0000

    added validation on form

--- src/Pages/Skills/index.js
+++ src/Pages/Skills/index.js
@@ -1,5 +1,95 @@
+import LinearProgress from "@mui/material/LinearProgress";
+import { useEffect } from "react";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+} from "react-icons/fa";
+import { GrMysql, GrGraphQl } from "react-icons/gr";
+import { SiMongodb, SiBootstrap } from "react-icons/si";
+
+const skills = [
+  {
+    id: "1",
+    icon: <FaHtml5 />,
+    skill: "HTML5",
+    percentage: "90%",
+  },
+  {
+    id: "2",
+    icon: <FaCss3 />,
+    skill: "CSS3",
+    percentage: "80%",
+  },
+  {
+    id: "3",
+    icon: <FaJsSquare />,
+    skill: "JAVASCRIPT",
+    percentage: "70%",
+  },
+  {
+    id: "4",
+    icon: <FaReact />,
+    skill: "REACT",
+    percentage: "80%",
+  },
+  {
+    id: "5",
+    icon: <FaNodeJs />,
+    skill: "NODE.JS",
+    percentage: "90%",
+  },
+  {
+    id: "5",
+    icon: <GrMysql />,
+    skill: "MySQL",
+    percentage: "90%",
+  },
+  {
+    id: "5",
+    icon: <SiMongodb />,
+    skill: "MongoDB",
+    percentage: "90%",
+  },
+  {
+    id: "5",
+    icon: <GrGraphQl />,
+    skill: "GraphQL",
+    percentage: "90%",
+  },
+  {
+    id: "5",
+    icon: <SiBootstrap />,
+    skill: "BootStrap",
+    percentage: "90%",
+  },
+  {
+    id: "5",
+    icon: <FaGithub />,
+    skill: "  GitHub",
+    percentage: "90%",
+  },
+];
+
 export const Skills = () => {
-  return <h1>Welcome to Skills</h1>;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("This will run every second!");
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <LinearProgress
+      sx={{ mt: "5rem" }}
+      color="primary"
+      value={30}
+      valueBuffer={100}
+      variant="determinate"
+    />
+  );
 };
 
 export default Skills;
--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
@@ -27,56 +28,110 @@ const InputField = withStyles({
 })(TextField);
 
 export const ContactForm = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Box component="div">
       <Grid justifyContent="center">
-        <Box component="form">
-          <Typography
-            sx={{
-              margin: "1rem 0",
-              color: "#979dac",
-              fontFamily: "Bebas Neue, cursive",
-              fontSize: "2rem",
-            }}
-          >
-            Get in Touch
-          </Typography>
+        <Typography
+          sx={{
+            margin: "1rem 0",
+            color: "#979dac",
+            fontFamily: "Bebas Neue, cursive",
+            fontSize: "2rem",
+          }}
+        >
+          Get in Touch
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)}>
           <InputField
+            {...register("name", { required: true })}
             fullWidth={true}
+            id="name"
+            name="name"
             label="Enter your name"
             variant="outlined"
             inputProps={{ style: { color: "#ffffff" } }}
             margin="dense"
             size="medium"
           ></InputField>
+          {errors.name?.type === "required" && (
+            <Typography
+              variant="subtitle2"
+              sx={{ color: "#0466c8ff", fontFamily: "Bebas Neue, cursive" }}
+            >
+              Name is required
+            </Typography>
+          )}
           <br />
           <InputField
+            {...register("email", {
+              required: true,
+            })}
             fullWidth={true}
+            id="email"
+            name="email"
             label="Enter your email"
             variant="outlined"
             inputProps={{ style: { color: "#ffffff" } }}
             margin="dense"
             size="medium"
           ></InputField>
-          <br />
+          {errors.email?.type === "required" && (
+            <Typography
+              variant="subtitle2"
+              sx={{ color: "#0466c8ff", fontFamily: "Bebas Neue, cursive" }}
+            >
+              Email is required
+            </Typography>
+          )}
+
           <InputField
+            {...register("subject", { required: true })}
             fullWidth={true}
+            id="subject"
+            name="subject"
             label="Enter the subject"
             variant="outlined"
             inputProps={{ style: { color: "#ffffff" } }}
             margin="dense"
             size="medium"
           ></InputField>
+          {errors.subject?.type === "required" && (
+            <Typography
+              variant="subtitle2"
+              sx={{ color: "#0466c8ff", fontFamily: "Bebas Neue, cursive" }}
+            >
+              Subject is required
+            </Typography>
+          )}
           <br />
           <InputField
+            {...register("message", { required: true })}
             fullWidth={true}
             multiline={true}
+            id="message"
+            name="message"
             label="Enter your message"
             variant="outlined"
             inputProps={{ style: { color: "#ffffff" } }}
             margin="dense"
             size="large"
           ></InputField>
+          {errors.message?.type === "required" && (
+            <Typography
+              variant="subtitle2"
+              sx={{ color: "#0466c8ff", fontFamily: "Bebas Neue, cursive" }}
+            >
+              Message is required
+            </Typography>
+          )}
           <br />
           <Box
             component="div"
@@ -85,6 +140,7 @@ export const ContactForm = () => {
           >
             <Button
               variant="outlined"
+              type="submit"
               endIcon={<SendIcon />}
               sx={{
                 marginTop: "0.5rem",

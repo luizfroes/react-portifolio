d9c383c3808368d6211b4c39f0edb438b2c899a6
Luiz Carlos Froes <luizfroes@gmail.com>
2022-05-07 00:43:47 +0100

    styled contact buttons

--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -5,8 +5,12 @@ import { ContactPage } from "../../components/ContactPage";
 
 export const Contact = () => {
   return (
-    <Container maxWidth={false} component="section">
-      <SectionTitle title="Contact Me" />
+    <Container
+      maxWidth={false}
+      component="section"
+      sx={{ width: "100%", height: "auto" }}
+    >
+      <SectionTitle title="Get in Touch" />
       <Divider sx={{ color: "#979dac", margin: "1rem 0 2rem" }} />
       <ContactPage />
     </Container>
--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -11,153 +11,149 @@ import Email from "@mui/icons-material/Email";
 
 export const ContactPage = () => {
   return (
-    <Box component="div">
-      <Grid justifyContent="center">
-        <Typography
+    <Container
+      margin="2rem"
+      disableGutters="true"
+      maxWidth={false}
+      component="section"
+      sx={{ width: "auto", justifyContent: "center" }}
+    >
+      <Button
+        sx={{
+          margin: "2rem",
+          width: 200,
+          height: 150,
+          padding: 1,
+          borderRadius: 5,
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "#0466c8ff",
+          border: "1px solid #979dac",
+          "&:hover": {
+            backgroundColor: "#001219",
+          },
+        }}
+        target="_blank"
+        href="https://drive.google.com/file/d/1KXu656eKdQ8MJz7760Lo19G6CgyD91bQ/view?usp=sharing"
+      >
+        <Resume
           sx={{
-            margin: "1rem 0",
-            color: "#979dac",
-            fontFamily: "Bebas Neue, cursive",
-            fontSize: "2rem",
+            width: 80,
+            height: 80,
+            color: "#ffffff",
           }}
+        />
+        <Typography
+          variant="h5"
+          color="#ffffff"
+          fontFamily="Bebas Neue, cursive"
         >
-          Get in Touch
+          Download Resume
         </Typography>
-        <Container>
-          <Button
-            sx={{
-              width: 200,
-              height: 150,
-              padding: 1,
-              borderRadius: 5,
-              justifyContent: "space-around",
-              alignItems: "center",
-              flexDirection: "column",
-              backgroundColor: "#0466c8ff",
-              border: "1px solid #979dac",
-              "&:hover": {
-                backgroundColor: "primary.main",
-              },
-            }}
-            target="_blank"
-            href="https://drive.google.com/file/d/1KXu656eKdQ8MJz7760Lo19G6CgyD91bQ/view?usp=sharing"
-          >
-            <Resume
-              sx={{
-                width: 80,
-                height: 80,
-                color: "#ffffff",
-              }}
-            />
-            <Typography
-              variant="h5"
-              color="#ffffff"
-              fontFamily="Bebas Neue, cursive"
-            >
-              Download Resume
-            </Typography>
-          </Button>
-          <Button
-            sx={{
-              width: 200,
-              height: 150,
-              padding: 1,
-              borderRadius: 5,
-              justifyContent: "space-around",
-              alignItems: "center",
-              flexDirection: "column",
-              backgroundColor: "#0466c8ff",
-              border: "1px solid #979dac",
-              "&:hover": {
-                backgroundColor: "primary.main",
-              },
-            }}
-            target="_blank"
-            href="mailto:luizfroesgeo@gmail.com"
-          >
-            <Email
-              sx={{
-                width: 80,
-                height: 80,
-                color: "#ffffff",
-              }}
-            />
-            <Typography
-              variant="h5"
-              color="#ffffff"
-              fontFamily="Bebas Neue, cursive"
-            >
-              Email me
-            </Typography>
-          </Button>
-          <Button
-            sx={{
-              width: 200,
-              height: 150,
-              padding: 1,
-              borderRadius: 5,
-              justifyContent: "space-around",
-              alignItems: "center",
-              flexDirection: "column",
-              backgroundColor: "#0466c8ff",
-              border: "1px solid #979dac",
-              "&:hover": {
-                backgroundColor: "primary.main",
-              },
-            }}
-            target="_blank"
-            href="https://www.linkedin.com/in/luiz-froes"
-          >
-            <LinkedIn
-              sx={{
-                width: 80,
-                height: 80,
-                color: "#ffffff",
-              }}
-            />
-            <Typography
-              variant="h5"
-              color="#ffffff"
-              fontFamily="Bebas Neue, cursive"
-            >
-              Visit my LinkedIn
-            </Typography>
-          </Button>
-          <Button
-            sx={{
-              width: 200,
-              height: 150,
-              padding: 1,
-              borderRadius: 5,
-              justifyContent: "space-around",
-              alignItems: "center",
-              flexDirection: "column",
-              backgroundColor: "#0466c8ff",
-              border: "1px solid #979dac",
-              "&:hover": {
-                backgroundColor: "primary.main",
-              },
-            }}
-            target="_blank"
-            href="https://github.com/luizfroes"
-          >
-            <GitHub
-              sx={{
-                width: 80,
-                height: 80,
-                color: "#ffffff",
-              }}
-            />
-            <Typography
-              variant="h5"
-              color="#ffffff"
-              fontFamily="Bebas Neue, cursive"
-            >
-              Visit my GitHub
-            </Typography>
-          </Button>
-        </Container>
-      </Grid>
-    </Box>
+      </Button>
+      <Button
+        sx={{
+          margin: "2rem",
+          width: 200,
+          height: 150,
+          padding: 1,
+          borderRadius: 5,
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "#0466c8ff",
+          border: "1px solid #979dac",
+          "&:hover": {
+            backgroundColor: "primary.main",
+          },
+        }}
+        target="_blank"
+        href="mailto:luizfroesgeo@gmail.com"
+      >
+        <Email
+          sx={{
+            width: 80,
+            height: 80,
+            color: "#ffffff",
+          }}
+        />
+        <Typography
+          variant="h5"
+          color="#ffffff"
+          fontFamily="Bebas Neue, cursive"
+        >
+          Email me
+        </Typography>
+      </Button>
+      <Button
+        sx={{
+          margin: "2rem",
+          width: 200,
+          height: 150,
+          padding: 1,
+          borderRadius: 5,
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "#0466c8ff",
+          border: "1px solid #979dac",
+          "&:hover": {
+            backgroundColor: "primary.main",
+          },
+        }}
+        target="_blank"
+        href="https://www.linkedin.com/in/luiz-froes"
+      >
+        <LinkedIn
+          sx={{
+            width: 80,
+            height: 80,
+            color: "#ffffff",
+          }}
+        />
+        <Typography
+          variant="h5"
+          color="#ffffff"
+          fontFamily="Bebas Neue, cursive"
+        >
+          Visit my LinkedIn
+        </Typography>
+      </Button>
+      <Button
+        sx={{
+          margin: "2rem",
+          width: 200,
+          height: 150,
+          padding: 1,
+          borderRadius: 5,
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundColor: "#0466c8ff",
+          border: "1px solid #979dac",
+          "&:hover": {
+            backgroundColor: "primary.main",
+          },
+        }}
+        target="_blank"
+        href="https://github.com/luizfroes"
+      >
+        <GitHub
+          sx={{
+            width: 80,
+            height: 80,
+            color: "#ffffff",
+          }}
+        />
+        <Typography
+          variant="h5"
+          color="#ffffff"
+          fontFamily="Bebas Neue, cursive"
+        >
+          Visit my GitHub
+        </Typography>
+      </Button>
+    </Container>
   );
 };
